9ea9a7e1d4e7e0099d434c357a398107ce59cdfb
Angelo <ben7152000@gmail.com>
2023-11-01 18:46:53 +0800

    fixed: 取消掉調試工具

--- js/window.js
+++ js/window.js
@@ -14,14 +14,3 @@ window.onkeydown = window.onkeyup = window.onkeypress = function (e) {
 document.addEventListener('contextmenu', function(e) {
   e.preventDefault();
 });
-
-/**
- * 禁止用調試工具
- */
-const threshold = 160
-const check = setInterval(function () {
-  if (window.outerWidth - window.innerWidth > threshold || window.outerHeight - window.innerHeight > threshold) {
-    window.location.reload()
-  }
-}, 1000)
-check()
