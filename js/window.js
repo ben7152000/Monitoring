a84a90782180f5de5d9480880184f7a16abe7c8a
Angelo <ben7152000@gmail.com>
2024-04-19 08:48:44 +0800

    fixed: 更換啟動動畫

--- js/window.js
+++ js/window.js
@@ -1,7 +1,7 @@
 /**
  * 禁止用F12
  */
-window.onkeydown = window.onkeyup = window.onkeypress = function (e) {
+window.onkeydown = window.onkeyup = window.onkeypress = (e) => {
   if (e.keyCode === 123) {
     e.preventDefault()
     window.event.returnValue = false
@@ -11,6 +11,25 @@ window.onkeydown = window.onkeyup = window.onkeypress = function (e) {
 /**
  * 禁止右鍵
  */
-document.addEventListener('contextmenu', function(e) {
+document.addEventListener('contextmenu', (e) => {
   e.preventDefault();
 });
+
+/**
+ * 啟動動畫
+ */
+document.addEventListener('DOMContentLoaded', () => {
+  const startupAnimation = document.querySelector('.startup-animation')
+  const logo = document.querySelector('.logo')
+  const loginInfo = document.querySelector('.login-info')
+
+  setTimeout(() => {
+    startupAnimation.style.transform = "translate(-50%, -50%) scale(0.7)";
+  }, 2000);
+
+  startupAnimation.addEventListener("transitionend", function () {
+    startupAnimation.classList.add("hidden");
+    logo.style.display = 'block'
+    loginInfo.style.display = 'flex'
+  });
+})
