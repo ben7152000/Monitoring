bf616e578d011f0be828afaac9d9f64b0e39bf5c
Ben <ben7152000@gmail.com>
2024-05-02 15:27:46 +0800

    feat: 添加新帳號

--- js/index.js
+++ js/index.js
@@ -76,6 +76,7 @@ let randomCount = 0 // 隨機產生的數量
 let alreadyCheck = false
 let catchBalls = []
 let removeBalls = []
+let users = []
 const userInfo = { account: '', password: '' }
 
 /**
@@ -119,10 +120,13 @@ function goToResultPage() {
  * 登入
  */
 function loginHandler() {
-  const userAccount = account.value
-  const userPassword = password.value
+  const _user = users.find(user => {
+    if (user.account === account.value) {
+      return user
+    }
+  })
 
-  if (userAccount === userInfo.account && userPassword === userInfo.password) {
+  if (_user && _user.password === password.value) {
     goToInstructionPage()
     loginInfo.style.display = NONE
   } else {
@@ -353,8 +357,15 @@ const key = 'AIzaSyCRhiUOa03yd0PobVYEnm5Ch0yXjFh9hww'
 fetch(`${url}/${id}/values/${AccountSheet}?alt=json&key=${key}`)
   .then(res => res.json())
   .then(res => {
-    userInfo.account = res.values[1][1]
-    userInfo.password = res.values[1][2]
+    const keys = res.values[0]
+    const result = res.values.slice(1).map(row => {
+      const obj = {}
+      keys.forEach((key, index) => {
+        obj[key.toLowerCase()] = row[index]
+      })
+      return obj
+    })
+    users = result
   })
 
 fetch(`${url}/${id}/values/${ParamsSheet}?alt=json&key=${key}`)
